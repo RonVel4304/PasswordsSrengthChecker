46126e2f7c74ae9c833b166c9d0ace64eb232359
Ronald Velasco <155396751+RonVel4304@users.noreply.github.com>
2024-07-10 16:08:23 -0700

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,20 @@
+# Basic Password Strength Checker
+
+# Lengths
+# 12 characters or fewer = 1 point ✓
+# 13 characters or more = 2 points ✓
+# 20 characters or more = 4 points ✓
+
+# Capitalization and Special Characters
+# Each capitalization earns 1 point ✓
+# Each number earns 1 point ✓
+# Each special character earns 4 points ✓
+
+# User can keep putting in test cases
+
+# Testing Cases from my password generator
+# Extreme: "?aDoRabLeaCcounTAnT84!" is 26 points
+# Hard:  "ElegantStudent84." is 12 points
+# Easy: "AgreeableLawyer97" is 9 points
+
+# Fun Easter egg, any password with the word 'password' is insanely strong
